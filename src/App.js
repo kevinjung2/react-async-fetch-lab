ab3f1f805e62c13631b57b4fe72edc7dff599dd9
Kevin Jung <kev.jung.ii@gmail.com>
2021-06-01 14:46:38 -0400

    commit tree 5c5910b75bab86b590ca47959a29c243338523e5

--- src/App.js
+++ src/App.js
@@ -1,1 +1,23 @@
 // create your App component here
+import React, { Component } from 'react'
+
+export default class App extends Component {
+
+  state = {
+    astros: []
+  }
+
+  componentDidMount() {
+    fetch('http://api.open-notify.org/astros.json')
+    .then(resp => resp.json())
+    .then(json => this.setState({astros: json.people}))
+  }
+
+  astros = () => {
+    return this.state.astros.map(astro => astro.name)
+  }
+
+  render() {
+    return <div>{this.astros()}</div>
+  }
+}
